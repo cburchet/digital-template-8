1be6de8365f32633c1dda3bf724922b03c188586
cburchet <cburchet@gmu.edu>
2015-04-08 16:11:32 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ window.onload = function() {
     
     function preload()
     {
-        game.load.image('road', 'assets/road.jpg');
         game.load.image('car', 'assets/car.png');
         game.load.image('redcar', 'assets/redbadcar.png');
         game.load.image('bluecar', 'assets/bluebadcar.png');
@@ -30,7 +29,6 @@ window.onload = function() {
         game.physics.startSystem(Phaser.Physics.ARCADE);
         game.physics.arcade.checkCollision.up = false;
         
-        background = game.add.tileSprite(0, 0, 1000, 600, 'road');
         
         player = game.add.sprite(400, game.world.height - 160, 'car');
         game.physics.arcade.enable(player);
@@ -39,12 +37,18 @@ window.onload = function() {
     	playerEngine.play('', 0, .1, true);
     	
     	greenOpponent = game.add.sprite(200, game.world.height - 160, 'greencar');
+    	game.physics.arcade.enable(greenOpponent);
     	redOpponent = game.add.sprite(300, game.world.height - 160, 'redcar');
+    	game.physics.arcade.enable(redOpponent);
     	blueOpponent = game.add.sprite(500, game.world.height - 160, 'bluecar');
+    	game.physics.arcade.enable(blueOpponent);
     }
     
     function update() 
     {
         game.physics.arcade.moveToPointer(player, speed, game.input.activePointer);
+        greenOpponent.velocity.y = -90;
+        redOpponent.velocity.y = -90;
+        blueOpponent.velocity.y = -90;
     }
 };
